b3682a2a49bd70ef06ebbc6b5abbce3568dbf70f
kbakkes <karim.bakkes@gmail.com>
2018-05-31 11:54:43 +0200

    added detailscreen

---
+++ screens/DetailScreen.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Text, View } from 'react-native';
+
+export default class DetailScreen extends React.Component {
+
+
+    render(){
+        return(
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Home Screen</Text>
+                <Button
+                    title="Profile"
+                    onPress={() => this.props.navigation.navigate('Profile',{title: 'My Profile'})}
+                />
+                <Button
+                    title="My plate"
+                    onPress={() => this.props.navigation.navigate('Plate',{title: 'My Plate'})}
+                />
+            </View>
+        )
+    }
+}
