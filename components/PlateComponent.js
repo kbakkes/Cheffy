f979349b01045ee76224cc63e7106fa8aacc1ecc
kbakkes <karim.bakkes@gmail.com>
2018-06-04 12:41:06 +0200

    added ingredient and category scrollers

--- components/PlateComponent.js
+++ components/PlateComponent.js
@@ -4,7 +4,7 @@ import { Image } from 'react-native';
 export default class PlateComponent extends Component {
     render() {
         return (
-            <Image source={require('./../images/plate.png')} style={{width: 300, height: 300, justifyContent: 'center'}}/>
+            <Image source={require('./../images/plate.png')} style={{width: 330, height: 330, justifyContent: 'center'}}/>
         );
     }
 }
--- screens/PlateScreen.js
+++ screens/PlateScreen.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import {Text, View, Button, Image, ScrollView } from 'react-native';
+import {Text, View, Button, Image, ScrollView,TouchableOpacity } from 'react-native';
 import PlateComponent from "../components/PlateComponent";
+import IngredientComponent from "../components/IngredientComponent";
 
 
 export default class PlateScreen extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            category: '',
+        }
+    }
+
     static navigationOptions = ({ navigation }) => ({
         title: `${navigation.state.params.title}`,
         headerTitleStyle : {textAlign: 'center',alignSelf:'center'},
@@ -13,20 +21,54 @@ export default class PlateScreen extends React.Component {
     });
 
     stringifyIngredients(){
+        console.log(this.state);
         return 'salmon';
     }
 
+
+    setCategoryState(category){
+        this.setState({
+           category: category
+        });
+        console.log(this.state);
+    }
+
+
+    returnCaterogies(){
+        return(
+            <ScrollView
+            horizontal={true} >
+                <TouchableOpacity onPress={() =>this.setCategoryState('beef')}>
+                    <Image source={require('./../images/icons/iconMeat.png')} style={{width: 100, height: 100}}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() =>this.setCategoryState('chicken')}>
+                    <Image source={require('./../images/icons/iconChicken.png')} style={{width: 100, height: 100}}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() =>this.setCategoryState('fish')}>
+                    <Image source={require('./../images/icons/iconFish.png')} style={{width: 100, height: 100}}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() =>this.setCategoryState('bread')}>
+                    <Image source={require('./../images/icons/iconBread.png')} style={{width: 100, height: 100}}/>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={() =>this.setCategoryState('herbs')}>
+                    <Image source={require('./../images/icons/iconCheese.png')} style={{width: 100, height: 100}}/>
+                </TouchableOpacity>
+
+
+            </ScrollView>
+        )
+    }
 
     render(){
         console.log(this.stringifyIngredients());
         return(
             <View>
-                <ScrollView
-                    horizontal={true} >
-                    <Image source={require('./../images/beef.png')} style={{width: 100, height: 100}}/>
-                    <Image source={require('./../images/patty.png')} style={{width: 100, height: 100}}/>
-                    <Image source={require('./../images/sausage.png')} style={{width: 100, height: 100}}/>
-                </ScrollView>
+                {this.returnCaterogies()}
+                <IngredientComponent category={this.state.category}/>
                 <PlateComponent  style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }} />
                 <Button
                     title="Search Recipes"
